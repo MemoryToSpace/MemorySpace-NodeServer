18083336ee2dadf0442985f50a6edf317eb1fe55
Elyahu Anavi <elyahu631@gmail.com>
2024-07-29 10:11:00 +0300

    update mail format

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -15,23 +15,31 @@ export const sendMemoryByEmail = async (memoryId: string, email: string): Promis
   }
 
   const message = `
-  <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px; background-color: #f3f4f6;">
-    <h1 style="color: #4CAF50; text-align: center;">Your Memory</h1>
-    <p style="font-size: 16px; color: #333;">Here is the memory you requested:</p>
-    <div style="margin: 20px 0; padding: 10px; border-radius: 8px; background-color: #fff; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-      <p style="font-size: 14px; color: #555;"><strong>Text:</strong></p>
-      <p style="font-size: 18px; color: #000; background: #e0f7fa; padding: 10px; border-radius: 5px;">${memory.inputText}</p>
-    </div>
-    <div style="text-align: center; margin: 20px 0;">
-      <img src="${memory.imageUrl}" alt="Memory Image" style="max-width: 100%; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);" />
-    </div>
-    <p style="font-size: 14px; color: #666;">Thank you for using our service!</p>
-    <p style="font-size: 14px; color: #666;">Best regards,<br /><strong>The MemorySpace Team</strong></p>
-    <div style="text-align: center; margin-top: 30px;">
-      <a href="https://example.com" style="font-size: 14px; color: #fff; background-color: #4CAF50; padding: 10px 20px; text-decoration: none; border-radius: 5px;">Visit Our Website</a>
+  <div style="font-family: Arial, sans-serif; max-width: 80vw; margin: 0 auto; padding: 20px; background-color: #FFFFFF; overflow: hidden;">
+    <h1 style="color: #0000FF; text-align: center;">Here is the memory you requested with the image we created for you</h1>
+    <div style="border: 1px solid #ddd; border-radius: 10px; background-color: #f9f9f9; padding: 20px;">
+      <h2 style="color: #0000FF; text-align: center; font-weight: normal;">FROM <strong>MEMORY</strong> TO <strong>SPACE</strong> THROUGH <strong>AI</strong></h2>
+      <h3 style="color: #0000FF; text-align: center; font-weight: normal;">LIFE THROUGH <strong>ALGORITHM</strong></h3>
+      <h4 style="color: #0000FF; text-align: center; font-weight: normal;">Noa Peretz | BEZALEL Architecture</h4>
+      <div style="margin: 20px 0; padding: 20px; border-radius: 8px; background-color: #FCF6EA; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+        <h4 style="font-size: 16px; font-weight: bold; color: #0000FF;">Your Memory:</h4>
+        <p style="font-size: 14px; color: #0000FF;">${memory.inputText}</p>
+      </div>
+      <div style="text-align: center; margin: 20px 0;">
+        <img src="${memory.imageUrl}" alt="Memory Image" style="max-width: 100%; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);" />
+      </div>
+      <p style="font-size: 16px; color: #0000FF; text-align: center;">Thanks for being a part of my project</p>
+      <p style="font-size: 16px; color: #0000FF; text-align: center;"><strong>Noa Peretz</strong></p>
+      <div style="text-align: center; margin: 30px 0;">
+        <a href="https://example.com" style="font-size: 14px; color: #fff; background-color: #F8A679; padding: 10px 20px; text-decoration: none; border-radius: 5px;">Visit Our Website</a>
+      </div>
+      <p style="font-size: 14px; color: #0000FF; text-align: center;">
+        I'd love to hear about your experience with the project and answer any questions you might have.<br>
+        Feel free to contact me via <a href="https://www.linkedin.com/in/noa-peretz-26b348280/" style="color: #0000FF;">LinkedIn</a> or <a href="https://www.instagram.com/peretznoa/" style="color: #0000FF;">Instagram</a>.
+      </p>
     </div>
   </div>
-`;
+  `;
 
   await sendEmail({
     to: email,
